5f9e15ad0718b559e592dba88da04dd64af6554a
Zhansiya <z_zheldybay@kbtu.kz>
2025-10-02 10:48:13 +0500

    1

---
+++ 4.py
@@ -0,0 +1,12 @@
+#void print_hello() {
+#     cout << "Hello world";
+# }
+# def print_hello():
+#     print("hello world")
+#
+# print_hello()
+
+def a_pluse_b(a, b):
+    return a + b
+
+print(a_pluse_b(3, 5))
