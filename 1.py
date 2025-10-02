10230a712aadcdf10dc86bfcdb444d83944b3748
Zhansiya <z_zheldybay@kbtu.kz>
2025-10-02 10:32:37 +0500

    Error

--- 1.py
+++ 1.py
@@ -5,5 +5,10 @@ a.append("abc")
 n = len(b)
 #for i in range(n, 0, -1):
 #    print(i)
-for i in b:
-    print(i, type(i))
+#for i in b:
+#    print(i, type(i))
+for (int i = 0; i < n; ++ i) {
+
+}
+print(range(n))
+range(n) = (0...n)
