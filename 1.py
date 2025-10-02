3201936a0fabf18eee5a07aca8637744c3c210ca
Zhansiya <z_zheldybay@kbtu.kz>
2025-10-02 08:52:18 +0500

    Second

--- 1.py
+++ 1.py
@@ -3,5 +3,7 @@ a = [1, 2, "abc", 1.12]
 b = (1, 2, "abc", 1.12)
 a.append("abc")
 n = len(b)
-for i in range(n, 0, -1):
-    print(i)
+#for i in range(n, 0, -1):
+#    print(i)
+for i in b:
+    print(i, type(i))
