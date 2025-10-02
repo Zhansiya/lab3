7755581312e29ab03d9a38557636cf8dfbfb5daa
Zhansiya <z_zheldybay@kbtu.kz>
2025-10-02 10:52:38 +0500

    3

--- 4.py
+++ 4.py
@@ -10,4 +10,9 @@ def a_pluse_b(a, b):
     return a + b
 
 #print(a_pluse_b(3, 5))
-print(a_pluse_b(int(input()), int(input())))
+#print(a_pluse_b(int(input()), int(input())))
+
+def power_of_two(*, n):
+    return 2 ** n
+
+print(power_of_two(n = int(input())))
