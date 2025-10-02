9865aa5a67624eed8325b88b0326c09e089edc7e
Zhansiya <z_zheldybay@kbtu.kz>
2025-10-02 18:41:29 +0500

    1

---
+++ function/filter_prime.py
@@ -0,0 +1,15 @@
+def filter_prime(numbers):
+    def is_prime(n):
+        if n < 2:
+            return False
+        for i in range(2, int(n**0.5) + 1):
+            if n % i == 0:
+                return False
+        return True
+
+    return [n for n in numbers if is_prime(n)]
+
+
+nums = list(map(int, input().split()))
+primes = filter_prime(nums)
+print(primes)
